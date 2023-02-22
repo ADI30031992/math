bcd992a69e28a679c3cccd2c67a39a02301463ee
Aditya Agarwal <adityaagarwal3003@gmail.com>
2023-02-22 22:16:41 +0530

    cube root

---
+++ math.py
@@ -0,0 +1,8 @@
+#take an input number
+num = int(input("Enter the Number"))
+#calculate cube root
+cube_root = num ** (1
+/3)
+
+#display the output
+print("Cube root of ", str(num), " is ", str(cube_root))
